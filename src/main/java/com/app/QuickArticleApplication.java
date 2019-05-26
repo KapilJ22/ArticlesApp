7f773ed1e37e4526fe9aaecb5fc464c68936cc2d
dronekaps <thyblissom@gmail.com>
2019-05-26 09:24:11 -0400

    Updated POM to make WAR for Tomcat, added dependency for tomcat.

--- src/main/java/com/app/QuickArticleApplication.java
+++ src/main/java/com/app/QuickArticleApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class QuickArticleApplication {
+public class QuickArticleApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(QuickArticleApplication.class, args);
