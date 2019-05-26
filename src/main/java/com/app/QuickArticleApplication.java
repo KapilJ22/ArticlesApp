41de13286409fc60a061997681c10945a41bb2a3
dronekaps <thyblissom@gmail.com>
2019-05-26 18:58:29 -0400

    misc

--- src/main/java/com/app/QuickArticleApplication.java
+++ src/main/java/com/app/QuickArticleApplication.java
@@ -2,7 +2,8 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
 
 @SpringBootApplication
 public class QuickArticleApplication extends SpringBootServletInitializer {
